a62326a1e1503aa07b7b26c4aba6de33a90f5518
Mitali Nanda <97724334+Mitali-N@users.noreply.github.com>
2022-03-16 19:35:14 +0530

    Modified login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,37 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+const login_element = document.querySelector("form#login"); // Fill the selector for the login element in ""
 let login_text = login_element.innerText;
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let question = login_text.split('\n')[3]; // Use split and array operations on the login_text string to extract the question
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+// Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
+//let answer = null;
+let ques_array = question.split(' ');
+
+if (ques_array[1] == 'add')
+{
+    num1 = ques_array[2];
+    num2 = ques_array[4];
+    answer = parseInt(num1) + parseInt(num2);
+}
+else if (ques_array[1] == 'subtract')
+{
+    num1 = ques_array[2];
+    num2 = ques_array[4];
+    answer = parseInt(num1) - parseInt(num2);
+}
+else if (ques_array[1] == 'enter')
+{
+    if ( ques_array[2] == 'first')
+    {
+        answer = parseInt(ques_array[4]);
+    }
+    else if ( ques_array[2] == 'second')
+    {
+        answer = parseInt(ques_array[6]);
+    }
+}
+
+const captcha_input_element = document.querySelector("input#valuepkg3"); // Fill the selector for the captcha input element in ""
 captcha_input_element.value = answer;
